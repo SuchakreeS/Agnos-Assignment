27519c571b63e72af6269698ecdddafe30864a25
SuchakreeS <poomfail2013@gmail.com>
2026-08-05 09:21:06 +0700

    feat: Required field validation

--- hooks/useFormValidation.js
+++ hooks/useFormValidation.js
@@ -1,4 +1,60 @@
-// Form validation logic (email, phone, required fields, date). Implemented in Phase 2.
+// Form validation logic (email, phone, required fields, date).
 export function useFormValidation() {
-  return {};
+  const validateForm = (formData) => {
+    const errors = {}
+
+    // Required Field validation
+    if(!formData.firstName?.trim()) {
+      errors.firstName = "Require a Firstname"
+    }
+    if(!formData.lastName?.trim()) {
+      errors.firstName = "Require a Lastname"
+    }
+    if(!formData.dateOfBirth) {
+      errors.firstName = "Require Date of birth"
+    }
+    if(!formData.gender) {
+      errors.firstName = "Require a Gender"
+    }
+    if(!formData.address?.trim()) {
+      errors.firstName = "Require an Address"
+    }
+    if(!formData.preferredLanguage) {
+      errors.firstName = "Require a Preferred Language"
+    }
+    if(!formData.nationally) {
+      errors.firstName = "Require a Nationallity"
+    }
+
+    // Email validation
+    if(!formData.email?.trim()) {
+      errors.email = "Required Email"
+    } else{
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+      if(!emailRegex.test(formData.email)) {
+        errors.email = "Please provide a valid email"
+      }
+    }
+
+    // Tel. validation
+    if(!formData.phoneNumber?.trim()) {
+      errors.phoneNumber = "Required Phone Number"
+    } else {
+      const digits = formData.phoneNumber.replace(/\D/g,"")
+      if (digits.length < 10) {
+        errors.phoneNumber = "Phone number must be at least 10 digits"
+      }
+    }
+
+    // Emergeny contact validation
+    if(formData.eContactName?.trim() && !formData.eContactRelationship?.trim()) {
+      errors.eContactRelationship = "Please specify the relationship of Emergency contact"
+    }
+
+    return errors
+  }
+
+  return {
+    validateForm
+  }
 }
